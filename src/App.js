a107e392df1440b9adcf4d2afb5d5aa2ab69479b
Dylando22 <dyl2elite@gmail.com>
2022-09-17 21:56:47 -0600

    Made temp URL and webpage

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Webpage Coming Soon</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://Dylando22.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go to temporary webpage
         </a>
       </header>
     </div>
